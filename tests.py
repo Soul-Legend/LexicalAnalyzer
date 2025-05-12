d2fcc89c0a957fccd4308bf8b2ff0496f1dd9709
Pedro Henrique de Sena Trombini Taglialenha <phtaglialenha@gmail.com>
2025-05-11 23:03:58 -0300

    Arruma alguns casos de teste, mas existem outros falhando

--- tests.py
+++ tests.py
@@ -10,16 +10,16 @@ TEST_CASES = [
         "re_definitions": (
             "ID: [a-zA-Z]([a-zA-Z]|[0-9])*\n"
             "NUM: [1-9]([0-9])*|0\n"
-            "WS: ( |\t|\r|\n)+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "a1\n"
-            "0\n"
-            "teste2\n"
-            "21\n"
-            "alpha123\n"
-            "3444\n"
-            "a43teste"
+            "a1 "
+            "0 "
+            "teste2 "
+            "21 "
+            "alpha123 "
+            "3444 "
+            "a43teste "
         )
     },
     {
@@ -27,13 +27,13 @@ TEST_CASES = [
         "re_definitions": (
             "ER1: a?(a|b)+\n"
             "ER2: b?(a|b)+\n"
-            "WS: ( |\t|\r|\n)+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "aa\n"
-            "bbbba\n"
-            "ababab\n"
-            "bbbbb\n"
+            "aa "
+            "bbbba "
+            "ababab "
+            "bbbbb "
             "c" # Error case
         )
     },
@@ -42,16 +42,17 @@ TEST_CASES = [
         "re_definitions": (
             "NUM: [0-9]+([.][0-9]+)?\n"
             "PLUS: [+]\n"
+            "EQ: [=]\n"
             "MINUS: [-]\n"
             "MUL: [*]\n"
             "DIV: [/]\n"
             "LPAREN: [(]\n"
             "RPAREN: [)]\n"
             "ID: [a-zA-Z_][a-zA-Z0-9_]*\n"
-            "WS: [ \t\r\n]+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "var1 = (10 + 5.5) * 2 / x\n"
+            "var1 = ( 10 + 5.5 ) * 2 / x "
             "3 - 1"
         )
     },
@@ -67,15 +68,15 @@ TEST_CASES = [
             "SEMI: ;\n"
             "COMMENT_LINE: //([^\r\n])*\n" # %ignore not specified here, so it becomes a token
             "COMMENT_BLOCK: /*(.|[\r\n])*?*/ %ignore\n" # This complex one might need careful NFA construction
-            "WS: [ \t\r\n]+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "if x = 10; // assign x\n"
-            "else\n"
-            "  /* block comment\n"
-            "     y = 20;\n"
-            "  */\n"
-            "  y = 20;\n"
+            "if x = 10; // assign x "
+            "else "
+            "  /* block comment "
+            "     y = 20; "
+            "  */ "
+            "  y = 20; "
             "while z;"
         )
     },
@@ -86,10 +87,10 @@ TEST_CASES = [
             "CHAR_SQ: '([^'\\\\]|\\\\.)'\n"   # Single quoted char with escapes
             "ID: [a-z]+\n"
             "PUNCT: [.,!?;]\n"
-            "WS: [ \t\r\n]+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "hello \"world\\\"!\" 'a' 'b\\''\n"
+            "hello \"world\\\"!\" 'a' 'b\\'' "
             "test, punct!"
         )
     },
@@ -100,13 +101,13 @@ TEST_CASES = [
             "A_PLUS_B: a+b\n"
             "A_OPT_B: a?b\n"
             "C_OR_D_STAR: (c|d)*\n"
-            "WS: [ \t\r\n]+ %ignore"
+            r"WS: ( |\t|\r|\n)+ %ignore"
         ),
         "source_code": (
-            "b\n"      # A_STAR_B, A_OPT_B
-            "ab\n"     # A_STAR_B, A_PLUS_B, A_OPT_B
-            "aaab\n"   # A_STAR_B, A_PLUS_B
-            "cdcdc\n"  # C_OR_D_STAR
+            "b "      # A_STAR_B, A_OPT_B
+            "ab "     # A_STAR_B, A_PLUS_B, A_OPT_B
+            "aaab "   # A_STAR_B, A_PLUS_B
+            "cdcdc "  # C_OR_D_STAR
             "e"        # Error
         )
     }
